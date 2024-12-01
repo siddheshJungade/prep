2013ef002f803e03b7f168ab8cf6195485954c5b
Siddhesh Jungade <>
2024-12-01 17:42:46 +0530

    Sunday 1 Dec
    Signed-off-by: Siddhesh Jungade <>

--- foundation/sprint-1/CraftingPattern.java
+++ foundation/sprint-1/CraftingPattern.java
@@ -5,7 +5,26 @@ Description: Write a program to create different star patterns (e.g., pyramid, d
 Example:
 Input: patternType = "pyramid", height = 5
 Output:
+    *
+   ***
+  *****
+ *******
+*********
  */
+
+import java.util.Scanner;
+
 public class CraftingPattern {
-    
+    static void drawPeramit(int n) {
+        int i = 0;
+        while(i < n){
+            System.out.println(" ".repeat(n-i) + "*".repeat(i*2 + 1));
+            i++;
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        drawPeramit(num);
+    }   
 }
